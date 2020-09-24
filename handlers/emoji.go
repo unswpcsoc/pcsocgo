84b1ecc4f239530f66729b52970a43d0831659d8
VineBalloon <vincentchen2.033@gmail.com>
2020-09-24 19:45:39 +1000

    add log

--- handlers/emoji.go
+++ handlers/emoji.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"errors"
 	"fmt"
+	"log"
 	"math/rand"
 	"strings"
 	"time"
@@ -92,6 +93,7 @@ func (e *emojiCount) MsgHandle(ses *discordgo.Session, msg *discordgo.Message) (
 		out += fmt.Sprintf("%s : %d\n", emojiText, count)
 	}
 
+	log.Println(out)
 	return commands.NewSimpleSend(msg.ChannelID, out), nil
 }
 
